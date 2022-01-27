b3e5a781074af597c91f5259d8c141f0f35dbc02
Alicia Marrero Ravelo <alu0101221960@ull.edu.es>
2022-01-27 18:32:39 +0000

    Comments

--- src/main/java/packageclient/Cliente.java
+++ src/main/java/packageclient/Cliente.java
@@ -1,6 +1,14 @@
 package packageclient;
 
+/*! \brief Clase Cliente
+ * Una clase cliente
+ *
+ */
 public class Cliente {
+    //! Metodo suma.
+    /*!
+      Recibe dos enteros y devuelve su suma
+    */
     public int suma (int a, int b){
         return a+b;
     }
